0c8094701d2dd277950a51e2dbd6fb271971761f
XIAN-SHENG-576692 <chenxiansheng576692@gmail.com>
2026-05-20 03:10:20 +0000

    refactor(all)

--- init/env/gen_env.sh
+++ init/env/gen_env.sh
@@ -10,11 +10,7 @@ ENV_FILE="${ENV_DIR}/.env"
 # Parameters
 INIT_DIR="${ROOT_DIR}/init"
 mkdir -p "${INIT_DIR}"
-NPM_DIR="${ENV_DIR}/npm"
-mkdir -p "${NPM_DIR}"
-PYTHON_DIR="${ENV_DIR}/python"
-mkdir -p "${PYTHON_DIR}"
-PYTHON_VENV_DIR="${PYTHON_DIR}/venv"
+PYTHON_VENV_DIR="${ENV_DIR}/python_venv"
 mkdir -p "${PYTHON_VENV_DIR}"
 
 ENV=(
@@ -24,9 +20,7 @@ ENV=(
     INIT_DIR="${INIT_DIR}"
     INIT_ENV_DIR="${INIT_DIR}/env"
     INSTALL_TXT="${ENV_DIR}/install.txt"
-    NPM_DIR="${NPM_DIR}"
-    NPM_INSTALL_TXT="${NPM_DIR}"/install.txt
-    PYTHON_DIR="${PYTHON_DIR}"
+    NPM_INSTALL_TXT="${ENV_DIR}"/npm_install.txt
     PYTHON_REQUIREMENTS_TXT="${ENV_DIR}/python_requirements.txt"
     PYTHON_VENV_DIR="${PYTHON_VENV_DIR}"
     PYTHON_VENV_ACTIVATE="${PYTHON_VENV_DIR}/bin/activate"
--- init/env/gen_install_env.sh
+++ init/env/gen_install_env.sh
@@ -19,64 +19,6 @@ if [ "$#" -eq 0 ]; then
     usage
 fi
 
-CARGO_INSTALL_ARRAY=()
-INSTALL_ARRAY=()
-NPM_INSTALL_ARRAY=()
-PYTHON_REQUIREMENTS_ARRAY=()
-VSCODE_EXTENSIONS_ARRAY=()
-
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        -c|--cargo|--cargo-install)
-            shift
-            # Loop until the next argument is an option or we run out of arguments
-            while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
-                CARGO_INSTALL_ARRAY+=("$1")
-                shift
-            done
-            ;;
-        -h|--help)
-            usage
-            ;;
-        -i|--in|--install)
-            shift
-            # Loop until the next argument is an option or we run out of arguments
-            while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
-                INSTALL_ARRAY+=("$1")
-                shift
-            done
-            ;;
-        -n|--npm|--npm-install)
-            shift
-            # Loop until the next argument is an option or we run out of arguments
-            while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
-                NPM_INSTALL_ARRAY+=("$1")
-                shift
-            done
-            ;;
-        -p|--python|python-requirements)
-            shift
-            # Loop until the next argument is an option or we run out of arguments
-            while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
-                PYTHON_REQUIREMENTS_ARRAY+=("$1")
-                shift
-            done
-            ;;
-        -v|--vs|--vscode-extensions)
-            shift
-            # Loop until the next argument is an option or we run out of arguments
-            while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
-                VSCODE_EXTENSIONS_ARRAY+=("$1")
-                shift
-            done
-            ;;
-        *)
-            echo "Unknown option: $1"
-            usage
-            ;;
-    esac
-done
-
 # Configuration
 ROOT_DIR=$(cd "${SCRIPT_DIR}/../.."; pwd)
 ENV_DIR="${ROOT_DIR}/env"
@@ -86,49 +28,43 @@ ENV_FILE="${ENV_DIR}/.env"
 source "${ENV_FILE}"
 source "${TOOLS_DIR}/utils/join_by_string.sh"
 
-# Configuration cargo Install
-CARGO_INSTALL=()
-for item in ${CARGO_INSTALL_ARRAY[@]}; do
-    mapfile -t -O "${#CARGO_INSTALL[@]}" CARGO_INSTALL < <(grep -Ev '^#|^$' "${item}")
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -c|--cargo|--cargo-install)
+            FILE="${CARGO_INSTALL_TXT}"
+            ;;
+        -h|--help)
+            usage
+            ;;
+        -i|--in|--install)
+            FILE="${INSTALL_TXT}"
+            ;;
+        -n|--npm|--npm-install)
+            FILE="${NPM_INSTALL_TXT}"
+            ;;
+        -p|--python|python-requirements)
+            FILE="${PYTHON_REQUIREMENTS_TXT}"
+            ;;
+        -v|--vs|--vscode-extensions)
+            FILE="${VSCODE_EXTENSIONS_TXT}"
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+    FILE_ARRAY=()
+    shift
+    # Loop until the next argument is an option or we run out of arguments
+    while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
+        FILE_ARRAY+=("$1")
+        shift
+    done
+    FILE_ITEMS=()
+    for item in ${FILE_ARRAY[@]}; do
+        mapfile -t -O "${#FILE_ITEMS[@]}" FILE_ITEMS < <(grep -Ev '^#|^$' "${item}")
+    done
+    FILE_ITEMS=($(printf "%s\n" "${FILE_ITEMS[@]}" | awk '!x[$0]++'))
+    FILE_CONTEXT=$(join_by_string "\n" "${FILE_ITEMS[@]}")
+    echo -e ${FILE_CONTEXT} > "${FILE}"
 done
-CARGO_INSTALL=($(printf "%s\n" "${CARGO_INSTALL[@]}" | awk '!x[$0]++'))
-CARGO_INSTALL_CONTEXT=$(join_by_string "\n" "${CARGO_INSTALL[@]}")
-
-# Configuration Install
-INSTALL=()
-for item in ${INSTALL_ARRAY[@]}; do
-    mapfile -t -O "${#INSTALL[@]}" INSTALL < <(grep -Ev '^#|^$' "${item}")
-done
-INSTALL=($(printf "%s\n" "${INSTALL[@]}" | awk '!x[$0]++'))
-INSTALL_CONTEXT=$(join_by_string "\n" "${INSTALL[@]}")
-
-# Configuration npm Install
-NPM_INSTALL=()
-for item in ${NPM_INSTALL_ARRAY[@]}; do
-    mapfile -t -O "${#NPM_INSTALL[@]}" NPM_INSTALL < <(grep -Ev '^#|^$' "${item}")
-done
-NPM_INSTALL=($(printf "%s\n" "${NPM_INSTALL[@]}" | awk '!x[$0]++'))
-NPM_INSTALL_CONTEXT=$(join_by_string "\n" "${NPM_INSTALL[@]}")
-
-# Configuration Python requirements
-PYTHON_REQUIREMENTS=()
-for item in ${PYTHON_REQUIREMENTS_ARRAY[@]}; do
-    mapfile -t -O "${#PYTHON_REQUIREMENTS[@]}" PYTHON_REQUIREMENTS < <(grep -Ev '^#|^$' "${item}")
-done
-PYTHON_REQUIREMENTS=($(printf "%s\n" "${PYTHON_REQUIREMENTS[@]}" | awk '!x[$0]++'))
-PYTHON_REQUIREMENTS_CONTEXT=$(join_by_string "\n" "${PYTHON_REQUIREMENTS[@]}")
-
-# Configuration VSCode extensions
-VSCODE_EXTENSIONS=()
-for item in ${VSCODE_EXTENSIONS_ARRAY[@]}; do
-    mapfile -t -O "${#VSCODE_EXTENSIONS[@]}" VSCODE_EXTENSIONS < <(grep -Ev '^#|^$' "${item}")
-done
-VSCODE_EXTENSIONS=($(printf "%s\n" "${VSCODE_EXTENSIONS[@]}" | awk '!x[$0]++'))
-VSCODE_EXTENSIONS_CONTEXT=$(join_by_string "\n" "${VSCODE_EXTENSIONS[@]}")
-
-# Output
-echo -e ${CARGO_INSTALL_CONTEXT} > "${CARGO_INSTALL_TXT}"
-echo -e ${INSTALL_CONTEXT} > "${INSTALL_TXT}"
-echo -e ${NPM_INSTALL_CONTEXT} > "${NPM_INSTALL_TXT}"
-echo -e ${PYTHON_REQUIREMENTS_CONTEXT} > "${PYTHON_REQUIREMENTS_TXT}"
-echo -e ${VSCODE_EXTENSIONS_CONTEXT} > "${VSCODE_EXTENSIONS_TXT}"
