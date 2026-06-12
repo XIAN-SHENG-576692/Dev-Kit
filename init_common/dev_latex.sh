251fb62353a55d43dd7a41a8f955d1ef9adf7185
XIAN-SHENG-576692 <chenxiansheng576692@gmail.com>
2026-06-12 08:20:36 +0000

    feat(common) feat(llm)

--- init_common/dev_latex.sh
+++ init_common/dev_latex.sh
@@ -14,10 +14,8 @@ ${ROOT_DIR}/init/env/gen_install_txt.sh \
     --install \
     ${ROOT_DIR}/init/env/install/common.txt \
     ${ROOT_DIR}/init/env/install/dev_latex.txt \
-    ${ROOT_DIR}/init/env/install/llm.txt \
     --vscode-extensions \
     ${ROOT_DIR}/init/env/vscode_extensions/common.txt \
     ${ROOT_DIR}/init/env/vscode_extensions/dev_latex.txt \
-    ${ROOT_DIR}/init/env/vscode_extensions/llm.txt \
 
 ${ROOT_DIR}/init/setup.sh
--- init_common/init_llm.sh
+++ init_common/init_llm.sh
@@ -12,22 +12,12 @@ ${ROOT_DIR}/init/env/rm_install_txt.sh
 
 ${ROOT_DIR}/init/env/gen_install_txt.sh \
     --install \
-    ${ROOT_DIR}/init/env/install/dev_typst.txt \
-
-${ROOT_DIR}/init/setup.sh
-
-source ${ROOT_DIR}/init/others/install_rustc.sh
-
-# ==================================================
-${ROOT_DIR}/init/env/rm_install_txt.sh
-
-${ROOT_DIR}/init/env/gen_install_txt.sh \
-    --cargo-install \
-    ${ROOT_DIR}/init/env/cargo/dev_typst.txt \
-    --install \
     ${ROOT_DIR}/init/env/install/common.txt \
+    ${ROOT_DIR}/init/env/install/llm.txt \
+    --npm-install \
+    ${ROOT_DIR}/init/env/npm/llm.txt \
     --vscode-extensions \
     ${ROOT_DIR}/init/env/vscode_extensions/common.txt \
-    ${ROOT_DIR}/init/env/vscode_extensions/dev_typst.txt \
+    ${ROOT_DIR}/init/env/vscode_extensions/llm.txt \
 
 ${ROOT_DIR}/init/setup.sh
